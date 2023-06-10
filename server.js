4562dab5ee0df898fac4a233b0b0b10e8706271e
Auryx <lilbizb1@gmail.com>
2023-06-10 12:43:00 -0700

    dynamic index & capitalization

--- server.js
+++ server.js
@@ -7,7 +7,21 @@ app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!")
 })
 app.get("/drinks", (req, res) => {
-    res.render("index.ejs", {drinks})
+    const capitalDrinks = drinks
+    capitalDrinks.forEach((drink) => {
+        let unCapDrink = drink.name.split(' ')
+        for(let i =0; i< unCapDrink.length; i++){
+            unCapDrink[i] = unCapDrink[i].charAt(0).toUpperCase() + unCapDrink[i].slice(1)
+            // console.log("uncap for each", unCapDrink[i])
+        }
+        // console.log("FOR EACH CONSOLE LOG", unCapDrink)
+        drink.name = unCapDrink.join(' ')
+    })
+    // console.log("ROUTE CONSOLE LOG",capitalDrinks)
+    res.render("index.ejs", {drinks:capitalDrinks})
+})
+app.get("/drinks/:id", (req, res) => {
+    res.send(req.params.id)
 })
 // HEY, LISTEN!!
 app.listen(3000, () => {
