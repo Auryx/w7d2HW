beb9f9941b3928541ed34712286a82fabd70e432
Auryx <lilbizb1@gmail.com>
2023-06-10 11:58:37 -0700

    Set up index view

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!")
 })
 app.get("/drinks", (req, res) => {
-    res.send(drinks)
+    res.render("index.ejs", {drinks})
 })
 // HEY, LISTEN!!
 app.listen(3000, () => {
