83204141728218f1f83c6cb195cbd02e62ee7a26
Auryx <lilbizb1@gmail.com>
2023-06-10 13:20:26 -0700

    added food and menu functionality

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 const express = require("express")
 const app = express()
 const drinks = require("./models/drinks")
+const food = require("./models/food")
 // ROUTES
 app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!")
 })
-app.get("/drinks", (req, res) => {
+app.get("/menu", (req, res) => {
+    // DRINKS
     const capitalDrinks = drinks
     capitalDrinks.forEach((drink) => {
         let unCapDrink = drink.name.split(' ')
@@ -18,14 +20,36 @@ app.get("/drinks", (req, res) => {
         drink.name = unCapDrink.join(' ')
     })
     // console.log("ROUTE CONSOLE LOG",capitalDrinks)
-    res.render("index.ejs", {drinks:capitalDrinks})
+
+    // FOOD
+    const capFood = food
+    capFood.forEach((food) => {
+        let unCapFood = food.name.split(' ')
+        for(let i =0; i< unCapFood.length; i++){
+            unCapFood[i] = unCapFood[i].charAt(0).toUpperCase() + unCapFood[i].slice(1)
+            // console.log("uncap for each", unCapDrink[i])
+        }
+        // console.log("FOR EACH CONSOLE LOG", unCapDrink)
+        food.name = unCapFood.join(' ')
+    })
+    res.render("index.ejs", {drinks:capitalDrinks, food:capFood})
 })
 app.get("/drinks/:id", (req, res) => {
     const id = req.params.id
-    drinks [id].image = drinks[id].image + '.png'
+    drinks[id].image = drinks[id].image + '.png'
     // console.log(drinks[id].image)
-    res.render("show.ejs", {drinks, id})
+    let consumable = drinks
+    res.render("show.ejs", {consumable, id})
 })
+
+app.get("/food/:id", (req, res) => {
+    const id = req.params.id
+    food[id].image = food[id].image + '.png'
+    // console.log(drinks[id].image)
+    let consumable = food
+    res.render("show.ejs", {consumable, id})
+})
+
 // HEY, LISTEN!!
 app.listen(3000, () => {
     console.log("Listening on Port 3000")
