230c638cb64e5ffc2aafdda7801d6ee0f465afac
Auryx <lilbizb1@gmail.com>
2023-06-10 11:43:32 -0700

    Added practice database

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // IMPORTS
 const express = require("express")
 const app = express()
-
+const drinks = require("./models/drinks")
 // ROUTES
 app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!")
 })
-
+app.get("/drinks", (req, res) => {
+    res.send(drinks)
+})
 // HEY, LISTEN!!
 app.listen(3000, () => {
     console.log("Listening on Port 3000")
